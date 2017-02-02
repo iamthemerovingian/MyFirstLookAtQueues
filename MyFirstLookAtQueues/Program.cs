adc787cdb585376d394c0ddb05ddd92e1d5c0c46
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-02-02 16:54:06 +0700

    Queue with Array tested and working, but tail begins as -1, why?!?!?!

--- MyFirstLookAtQueues/Program.cs
+++ MyFirstLookAtQueues/Program.cs
@@ -10,13 +10,36 @@ namespace MyFirstLookAtQueues
     {
         static void Main(string[] args)
         {
+            Queue<int> queue = new Queue<int>();
+            QueueExperiement1(queue);
 
+            QueueArray<int> queueArray = new QueueArray<int>();
+            QueueExperiement2(queueArray);
         }
 
-        private static void FIFO_Experiement()
+        private static void QueueExperiement2(QueueArray<int> queue)
         {
-            Queue<int> queue = new Queue<int>();
-
+            queue.AddtoQueue(1);
+            queue.AddtoQueue(2);
+            queue.AddtoQueue(3);
+            queue.AddtoQueue(4);
+            queue.DeQueue();
+            queue.DeQueue();
+            queue.DeQueue();
+            queue.AddtoQueue(5);
+            queue.AddtoQueue(6);
+            queue.DeQueue();
+            queue.AddtoQueue(7);
+            queue.AddtoQueue(8);
+            queue.DeQueue();
+            queue.DeQueue();
+            queue.DeQueue();
+            queue.AddtoQueue(9);
+            queue.DeQueue();
+            queue.DeQueue();
+        }
+        private static void QueueExperiement1(Queue<int> queue)
+        {
             queue.AddtoQueue(1);
             queue.AddtoQueue(2);
             queue.AddtoQueue(3);
--- MyFirstLookAtQueues/QueueArray.cs
+++ MyFirstLookAtQueues/QueueArray.cs
@@ -1,32 +1,75 @@
-﻿namespace MyFirstLookAtQueues
+﻿
+namespace MyFirstLookAtQueues
 {
-    class QueueArray<T>
+    class QueueArray<T> : System.Collections.Generic.IEnumerable<T>
     {
         private T[] _array = new T[0];
 
         private int _head = 0;
-        private int _tail = 0;
+        private int _tail = -1;
 
         private int _size = 0;
 
-        public void Queue(T item)
+        public void AddtoQueue(T item)
         {
+
             if (_size == _array.Length)
             {
-                int newLength = _size == 0 ? 4 : _size * 2;
+                int targetIndex = 0;
 
+                int newLength = _size == 0 ? 4 : _size * 2;
                 T[] temp = new T[newLength];
-                if (_head > _tail)
+
+                if (_size > 0)
                 {
-                    _array.CopyTo(temp, 0);
-                    _array = temp;
+                   if (_tail < _head)
+                    {
+                        //Copy from head to the end of the array.
+                        for (int i = _head; i < _array.Length; i++)
+                        {
+                            temp[targetIndex] = _array[i];
+                            targetIndex++;
+                        }
+                        //copy from begening to tail
+                        for (int i = 0; i <= _tail; i++)
+                        {
+                            temp[targetIndex] = _array[i];
+                        }
+
+                    }
+                    else// Then the head is at the front of the array and the tail is at the end, so normal copying will suffice.
+                    {
+                        for (int i = 0; i <= _tail; i++)
+                        {
+                            temp[targetIndex] = temp[i];
+                            targetIndex++;
+                        }
+                    }
+                    _head = 0;
+                    _tail = targetIndex -1;
                 }
+                else
+                {
+                    _head = 0;
+                    _tail = -1;
+                }
+                
+                _array = temp;
 
             }
 
-            _array[_size] = item;
+            //Add the new item to the array.
+            if (_tail == _array.Length -1)
+            {
+                _tail = 0;
+            }
+            else
+            {
+                _tail++;
+            }
+
+            _array[_tail] = item;
             _size++;
-            _head++;
         }
 
         public T DeQueue()
@@ -37,10 +80,59 @@
             }
 
             T temp = _array[_head];
-            _head++;
+
+            //wrap the head
+            if (_head == _array.Length - 1)
+            {
+                _head = 0;
+            }
+            else
+            {
+                _head++;
+            }
+
             _size--;
 
+            System.Console.WriteLine(temp);
             return temp;
+        }
+
+        public void Clear()
+        {
+            _head = 0;
+            _tail = -1;
+            _size = 0;
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            if (_tail < _head)
+            {
+                // head to end.
+                for (int i = _head; i < _array.Length; i++)
+                {
+                    yield return _array[i];
+                }
+
+                //0 to tail.
+                for (int i = 0; i < _tail; i++)
+                {
+                    yield return _array[i];
+                }
+            }
+            else
+            {
+                //Head to tail.
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    yield return _array[i];
+                }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
